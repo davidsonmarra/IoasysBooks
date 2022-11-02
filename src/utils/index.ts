5c30fd787b878663574d0753c1a18ff7f76aa8a8
Davidson Marra <davidsonmarra@CPU-00335.local>
2022-11-02 17:17:43 -0300

    refactor(utils): refactor param on function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,6 @@
 import { AxiosError } from 'axios';
 
-export const isObjectEmpty = (prop: Record<string, unknown> | Error | AxiosError<unknown, any>) =>
+export const isObjectEmpty = (prop: Record<string, unknown> | Error | AxiosError<unknown>) =>
   Object.keys(prop).length > 0;
 
 export { categories } from './categories';
